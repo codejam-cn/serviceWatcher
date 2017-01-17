acbc619b9f48cc22ed08311ba51a162346b3d820
festone000 <festone000@hotmail.com>
2017-01-17 17:20:12 +0800

    添加项目文件。

---
+++ FreeProxyWatcher/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.ServiceProcess;
+
+namespace FreeProxyWatcher
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        private static void Main()
+        {
+            var ServicesToRun = new ServiceBase[]
+            {
+                new Service1()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
