0d75f4419a3454f7c1fa917709c4dc33b6a25730
festone000 <festone000@hotmail.com>
2017-01-18 15:46:21 +0800

    控制台项目，协助测试的。没什么意义。

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                string url = ConfigurationManager.AppSettings["watchIpAndPort"];
+                string serviceName = ConfigurationManager.AppSettings["serviceName"];
+
+                Process p = new Process();
+                p.StartInfo.FileName = "cmd.exe";//设置启动的应用程序  
+                p.StartInfo.UseShellExecute = false;//禁止使用操作系统外壳程序启动进程  
+                p.StartInfo.RedirectStandardInput = true;//应用程序的输入从流中读取  
+                p.StartInfo.RedirectStandardOutput = true;//应用程序的输出写入流中  
+                p.StartInfo.RedirectStandardError = true;//将错误信息写入流  
+                p.StartInfo.CreateNoWindow = true;
+                p.Start();
+                p.StandardInput.WriteLine(@"netstat -a -n");
+                bool isRunning = false;
+                while ((p.StandardOutput.ReadLine()) != null)
+                {
+                    var str = p.StandardOutput.ReadLine();
+                    if (str != null && str.IndexOf(url, StringComparison.Ordinal) != -1 && str.IndexOf("ESTABLISHED", StringComparison.Ordinal) != -1)
+                    {
+                        isRunning = true;
+                        break;
+                    }
+                }
+                p.StandardInput.Flush();
+                p.StandardInput.AutoFlush = true;
+                p.Start();
+                p.StandardInput.WriteLine("net {0} {1}", isRunning ? "start" : "stop", serviceName);
+                p.WaitForExit();
+                p.Close();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+
+
+        }
+
+
+    }
+}
--- FreeProxyWatcher/Service1.cs
+++ FreeProxyWatcher/Service1.cs
@@ -1,15 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Configuration;
-using System.Data;
 using System.Diagnostics;
-using System.Linq;
-using System.Net;
-using System.Net.NetworkInformation;
 using System.ServiceProcess;
-using System.Text;
-using System.Threading.Tasks;
 using System.Timers;
 
 namespace FreeProxyWatcher
@@ -26,14 +18,14 @@ namespace FreeProxyWatcher
             string intervalMs = ConfigurationManager.AppSettings["intervalMs"];
             int interval = int.Parse(intervalMs);
             var timer = new Timer(interval);
-            timer.Elapsed += Get;
+            timer.Elapsed += Run;
             timer.Start();
         }
 
 
-        public static void Get(object sender, ElapsedEventArgs e)
+        public static void Run(object sender, ElapsedEventArgs e)
         {
-            string[] secondRangeArr = ConfigurationManager.AppSettings["secondRange"].Split(new char[] { ',' });
+            string[] secondRangeArr = ConfigurationManager.AppSettings["secondRange"].Split(',');
             int minSec = int.Parse(secondRangeArr[0]);
             int maxSec = int.Parse(secondRangeArr[1]);
 
@@ -46,52 +38,34 @@ namespace FreeProxyWatcher
 
         public static void CheckFreeProxyServiceState()
         {
-            string url =  ConfigurationManager.AppSettings["watchIpAndPort"];
-
-            using (var myPro = new Process())
-            {
-                myPro.StartInfo.FileName = "netstat -a";
-                myPro.StartInfo.UseShellExecute = false;
-                myPro.StartInfo.RedirectStandardInput = true;
-                myPro.StartInfo.RedirectStandardOutput = true;
-                myPro.StartInfo.RedirectStandardError = true;
-                myPro.StartInfo.CreateNoWindow = true;
-                myPro.Start();
-                const string str = @"shutdown -f -s -t 300";
-                myPro.StandardInput.WriteLine(str);
-                myPro.StandardInput.AutoFlush = true;
-                myPro.WaitForExit();
-            }
-
-
-            Process p = new Process
-            {
-                StartInfo = new ProcessStartInfo("netstat", "-a")
-                {
-                    CreateNoWindow = true,
-                    UseShellExecute = false,
-                    WindowStyle = ProcessWindowStyle.Hidden,
-                    RedirectStandardOutput = true
-                }
-            };
+            string url = ConfigurationManager.AppSettings["watchIpAndPort"];
+            string serviceName = ConfigurationManager.AppSettings["serviceName"];
+            Process p = new Process();
+            p.StartInfo.FileName = "cmd.exe";//设置启动的应用程序  
+            p.StartInfo.UseShellExecute = false;//禁止使用操作系统外壳程序启动进程  
+            p.StartInfo.RedirectStandardInput = true;//应用程序的输入从流中读取  
+            p.StartInfo.RedirectStandardOutput = true;//应用程序的输出写入流中  
+            p.StartInfo.RedirectStandardError = true;//将错误信息写入流  
+            p.StartInfo.CreateNoWindow = true;
             p.Start();
-            string result = p.StandardOutput.ReadToEnd();
-            if (result.IndexOf(url, StringComparison.Ordinal) < 0)
+            p.StandardInput.WriteLine(@"netstat -a -n");
+            bool isRunning = false;
+            while ((p.StandardOutput.ReadLine()) != null)
             {
-                using (var myPro = new Process())
+                var str = p.StandardOutput.ReadLine();
+                if (str != null && str.IndexOf(url, StringComparison.Ordinal) != -1 && str.IndexOf("ESTABLISHED", StringComparison.Ordinal) != -1)
                 {
-                    myPro.StartInfo.FileName = "net start FreeProxy";
-                    myPro.StartInfo.UseShellExecute = false;
-                    myPro.StartInfo.RedirectStandardInput = true;
-                    myPro.StartInfo.RedirectStandardOutput = true;
-                    myPro.StartInfo.RedirectStandardError = true;
-                    myPro.StartInfo.CreateNoWindow = true;
-                    myPro.Start();
-
+                    isRunning = true;
+                    break;
                 }
             }
+            p.StandardInput.Flush();
+            p.StandardInput.AutoFlush = true;
+            p.Start();
+            p.StandardInput.WriteLine("net {0} {1}", isRunning ? "start" : "stop", serviceName);
+            p.WaitForExit();
+            p.Close();
         }
-
 
         protected override void OnStop()
         {
